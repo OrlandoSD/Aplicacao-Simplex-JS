54ae216c6668ed6e836a1db797a61117b4989310
OrlandoSD <py2osd.orlando@gmail.com>
2022-06-07 17:31:22 -0300

    mais um exemplo de variaveis

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const pessoa = {
     idade: 29,
     profissao: 'programadora'
 };
+const andre;
+const paulo;
+const paulo, { nome: , 'Paulo': , idade: , 25: , profissao: , 'Programadora':  };
+const paula, { nome: , 'Paulo': , idade: , 25: , profissao: , 'Engennheira': materias, ['Portugues , matematica']:  };
+function listar(lista) {
+    for (const item of lista) {
+        console.log(' - ', item);
+    }
+}
+listar(paula.materias);
--- app.ts
+++ app.ts
@@ -3,3 +3,46 @@ const pessoa = {
     idade: 29,
     profissao: 'programadora'
 }
+
+const andre: (nome: string, idade: number, profissao: string)  {
+    nome: 'André',
+    idade: 25,
+    profissao: 'pintor'
+}
+
+const paulo: (nome: string, idade: number, profissao: string)  {
+    nome: 'Paulo',
+    idade: 25,
+    profissao: 'Programadora'
+}
+
+interface Estudandte extends Pessoa {
+    materias: string[]
+}
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao: Profissao
+}
+
+const paulo : Pessoa {
+    nome: 'Paulo',
+    idade: 25,
+    profissao: 'Programadora'
+}
+
+const paula : Estudandte {
+    nome: 'Paulo',
+    idade: 25,
+    profissao: 'Engennheira'
+    materias: ['Portugues , matematica']
+}
+
+function listar(lista: string[]) {
+
+    for (const item of lista){
+        console.log(' - ', item);
+        }
+    }
+    listar(paula.materias);
